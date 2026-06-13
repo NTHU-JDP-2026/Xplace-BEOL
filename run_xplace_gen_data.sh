d8362d31bfc2c391661ce14746a51a079f5f5f31
JR <mm113501702@gapp.nthu.edu.tw>
2026-06-13 15:32:36 +0800

    fix: different pdn density

--- run_xplace_gen_data.sh
+++ run_xplace_gen_data.sh
@@ -2,7 +2,8 @@
 
 design_name=$1
 target_density=$2
-
+m3_reduction=$3
+cell_inflate=$4
 python3 /home/jiarui/Xplace/tool/generate_contest_json.py "$design_name"
 
 
@@ -25,7 +26,7 @@ docker exec compassionate_pascal bash -c \
 --load_from_raw True \
 --target_density ${target_density} \
 --use_m3_snet_density True \
---m3_density_reduction_factor 0.65 \
+--m3_density_reduction_factor ${m3_reduction} \
 --output_dir ${design_name} \
 --write_global_placement True \
 --use_cell_inflate True \
